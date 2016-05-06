ff5aa0ec6877516daa9e79bf02f1a66284baca07
unknown <beck.andras@989096CE5198.phgroup.local>
2016-05-06 10:18:53 +0200

    fix

--- src/Andrewboy/GoogleAnalyticsAdminPanel/GoogleAnalyticsAdminPanelServiceProvider.php
+++ src/Andrewboy/GoogleAnalyticsAdminPanel/GoogleAnalyticsAdminPanelServiceProvider.php
@@ -1,4 +1,4 @@
-<?php namespace \Andrewboy\GoogleAnalyticsAdminPanel;
+<?php namespace Andrewboy\GoogleAnalyticsAdminPanel;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -33,24 +33,32 @@ class GoogleAnalyticsAdminPanelServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        $resourcesDir =  __DIR__ . '/../../../resources';
+
         #VIEW
-        $this->loadViewsFrom(__DIR__ . '/../../../resources/views/google_analytics_statistics/', 'google-analytics-statistics');
+        $this->loadViewsFrom(
+            $resourcesDir .'/views/google_analytics_statistics/',
+            'google-analytics-statistics'
+        );
         $this->publishes([
-            __DIR__. '/../../../resources/views/google_analytics_statistics/' => base_path('resources/views/vendor/google-analytics-statistics')
+            $resourcesDir .'/views/google_analytics_statistics/' => base_path('resources/views/vendor/google-analytics-statistics')
         ]);
 
         #TRANSLATIONS
-        $this->loadTranslationsFrom(__DIR__ . '/../../../resources/lang/', 'google-analytics-statistics');
+        $this->loadTranslationsFrom(
+            $resourcesDir .'/lang/',
+            'google-analytics-statistics'
+        );
         $this->publishes(
             [
-                __DIR__ . '/../../../resources/lang/' => base_path('resources/lang/vendor/google-analytics-statistics'),
+                $resourcesDir .'/lang/' => base_path('resources/lang/vendor/google-analytics-statistics'),
             ]
         );
 
         #ASSETS
         $this->publishes(
             [
-                __DIR__ . '/../../../assets/' => public_path('vendor/google-analytics-statistics'),
+                $resourcesDir .'/assets/' => public_path('vendor/google-analytics-statistics'),
             ],
             'public'
         );
